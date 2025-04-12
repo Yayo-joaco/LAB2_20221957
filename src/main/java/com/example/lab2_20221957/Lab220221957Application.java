e79f99636555396f5ce1ef5d188214c57debd825
Yayo_joaco <gjrabanal@gmail.com>
2025-04-11 20:27:02 -0500

    first commit

---
+++ src/main/java/com/example/lab2_20221957/Lab220221957Application.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20221957;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab220221957Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220221957Application.class, args);
+    }
+
+}
